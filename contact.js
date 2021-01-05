e75be72dfdba26d86408968b364a92f954684a32
DanielleE1998 <60410676+DanielleE1998@users.noreply.github.com>
2021-01-05 16:52:07 -0500

    file upload

---
+++ contact.js
@@ -0,0 +1,78 @@
+"use strict";
+var $ = function(id) {
+	return document.getElementById(id);
+};
+
+
+var processEntries = function() {
+	var fname = ($("fname").value);
+	var lname = ($("lname").value);
+	var mail = ($("mail").value);
+	var validEmail =/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/; //Regular Expression
+	var subject = ($("subject").value);
+	var comments = ($("comments").value);
+	
+
+	var error1 ="*";
+	var error2 ="*";
+	var error3 ="*";
+		
+	/*check for errors*/
+	if(fname == "" || lname == "" || mail == "" || validEmail.test(mail) == false){
+		/*validate first name*/
+		if(fname == ""){
+			error1 = "First name is required.";
+		}
+		else {
+			var error1 = ""; 
+		}
+		$("error1").firstChild.nodeValue = error1;
+
+		/*validate last name*/
+		if(lname == ""){
+			error2 = "Last name is required.";
+		}
+		else {
+			var error2 = ""; 
+		}
+		$("error2").firstChild.nodeValue = error2;
+
+		/*validate email*/
+		if(mail == ""){
+			error3 = "Email is required.";
+		}
+		else if(validEmail.test(mail) == false){
+			error3= "Not a valid email."
+		}
+		else {
+			var error3 = ""; 
+		}
+		$("error3").firstChild.nodeValue = error3;
+	}
+	else{
+		var error1 = ""; 
+		var error2 = ""; 
+		var error3 = ""; 
+	}
+};
+
+var clearEntries = function() {
+	$("fname").value = "";
+	$("lname").value = "";
+	$("mail").value = "";
+	$("subject").value = "general";
+	$("comments").value = "";
+	var error1 ="*";
+	var error2 ="*";
+	var error3 ="*";
+	$("error1").firstChild.nodeValue = error1;
+	$("error2").firstChild.nodeValue = error2;
+	$("error3").firstChild.nodeValue = error3;
+};
+
+
+window.onload = function() {
+	$("submit").onclick = processEntries;
+	$("reset").onclick = clearEntries;
+};
+
---
+++ projects.js
@@ -0,0 +1,142 @@
+var slideIndex1 = 1;
+var slideIndex2 = 1;
+var slideIndex3 = 1;
+var slideIndex4 = 1;
+document.addEventListener("DOMContentLoaded", function(event) {
+showDivs1(slideIndex1);
+showDivs2(slideIndex2);
+showDivs3(slideIndex3);
+showDivs4(slideIndex4);
+});
+
+//Arrow Functions_____________________________
+
+function plusDivs1(n) {
+  showDivs1(slideIndex1 += n);
+}
+
+function plusDivs2(n) {
+  showDivs2(slideIndex2 += n);
+}
+
+function plusDivs3(n) {
+  showDivs3(slideIndex3 += n);
+}
+
+function plusDivs4(n) {
+  showDivs4(slideIndex4 += n);
+}
+
+//Button Functions_____________________________
+function currentDiv1(n) {
+  showDivs1(slideIndex1 = n);
+}
+
+function currentDiv2(n) {
+  showDivs2(slideIndex2 = n);
+}
+
+function currentDiv3(n) {
+  showDivs3(slideIndex3 = n);
+}
+
+function currentDiv4(n) {
+  showDivs4(slideIndex4 = n);
+}
+
+//Update Functions_____________________________
+function showDivs1(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides1"); 
+  var dots = document.getElementsByClassName("demo"); 
+
+  if (n > x.length) {slideIndex1 = 1} //cycle to first
+  if (n < 1) {slideIndex1 = x.length} //cycle to last
+
+  //hide images
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+
+  //hids dots
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  console.log(slideIndex1);
+  console.log(x.length);
+  //show image and dot at slide index
+  x[slideIndex1-1].style.display = "block";  
+  dots[slideIndex1-1].className += " w3-white";
+}
+
+function showDivs2(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides2"); 
+  var dots = document.getElementsByClassName("demo"); 
+
+  if (n > x.length) {slideIndex2 = 1} //cycle to first
+  if (n < 1) {slideIndex2 = x.length} //cycle to last
+
+  //hide images
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+
+  //hids dots
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  console.log(slideIndex2);
+  console.log(x.length);
+  //show image and dot at slide index
+  x[slideIndex2-1].style.display = "block";  
+  dots[slideIndex2-1].className += " w3-white";
+}
+
+function showDivs3(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides3"); 
+  var dots = document.getElementsByClassName("demo"); 
+
+  if (n > x.length) {slideIndex3 = 1} //cycle to first
+  if (n < 1) {slideIndex3 = x.length} //cycle to last
+
+  //hide images
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+
+  //hids dots
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  console.log(slideIndex3);
+  console.log(x.length);
+  //show image and dot at slide index
+  x[slideIndex3-1].style.display = "block";  
+  dots[slideIndex3-1].className += " w3-white";
+}
+
+function showDivs4(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides4"); 
+  var dots = document.getElementsByClassName("demo"); 
+
+  if (n > x.length) {slideIndex4 = 1} //cycle to first
+  if (n < 1) {slideIndex4 = x.length} //cycle to last
+
+  //hide images
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+
+  //hids dots
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  console.log(slideIndex4);
+  console.log(x.length);
+  //show image and dot at slide index
+  x[slideIndex4-1].style.display = "block";  
+  dots[slideIndex4-1].className += " w3-white";
+}
